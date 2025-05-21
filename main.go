f3a6eda3c46328cc54fbbc8f45d2d02af796cae3
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2025-05-21 16:06:37 +0700

    add not found handler and panic handler

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 
 func main() {
 	router := httprouter.New()
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, error interface{}) {
+		fmt.Fprint(w, "Panic : ", error)
+	}
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Sorry, we can't find the page you looking for")
+	})
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		// panic("Opps, something went wrong!")
 		fmt.Fprint(w, "Hello http router")
 	})
 
