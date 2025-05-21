f0ca9bfc4e3536dd0389578a213ddbc347480053
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2025-05-21 15:55:08 +0700

    add file serve

--- router_test.go
+++ router_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -76,4 +78,29 @@ func TestRouterPatternCatchAllParams(t *testing.T) {
 	body, _ := io.ReadAll(response.Body)
 	fmt.Println(string(body))
 	assert.Equal(t, "Images /small/profile.png", string(body))
+}
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	directory, err := fs.Sub(resources, "resources")
+	if err != nil {
+		fmt.Println(err)
+	}
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	fmt.Println(response)
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Equal(t, "Hello HttpRouter", string(body))
 }
